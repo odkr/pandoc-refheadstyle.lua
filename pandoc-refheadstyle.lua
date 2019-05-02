3ae17f2659c9542ba717478a017c29cb346c5c72
Odin Kroeger <odkr@users.noreply.github.com>
2019-05-02 15:50:28 +0200

    Bumped up version number.

--- pandoc-refheadstyle.lua
+++ pandoc-refheadstyle.lua
@@ -1,7 +1,7 @@
 --- Sets the style of the reference section header.
 --
 -- @script pandoc-refheadstyle.lua
--- @release 0.2.7a
+-- @release 0.2.7
 -- @author Odin Kroeger
 -- @copyright 2018 Odin Kroeger
 -- @license MIT
